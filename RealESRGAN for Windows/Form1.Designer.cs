4dbba844766696e1ac265ce085c8f9276ba2987c
安 <2222002781@qq.com>
2022-06-08 14:59:19 +0800

    [UPDATE]Ver1.0.3

---
+++ RealESRGAN for Windows/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace RealESRGAN_for_Windows
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.picPath = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.radioButtonAnime = new System.Windows.Forms.RadioButton();
+            this.radioButtonDefault = new System.Windows.Forms.RadioButton();
+            this.animeVideoX2 = new System.Windows.Forms.RadioButton();
+            this.animeVideoX4 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.formatChecked = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.upscaleChecked = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // picPath
+            // 
+            this.picPath.BackColor = System.Drawing.SystemColors.Window;
+            resources.ApplyResources(this.picPath, "picPath");
+            this.picPath.Name = "picPath";
+            this.picPath.ReadOnly = true;
+            this.picPath.TabStop = false;
+            this.picPath.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // radioButtonAnime
+            // 
+            resources.ApplyResources(this.radioButtonAnime, "radioButtonAnime");
+            this.radioButtonAnime.Checked = true;
+            this.radioButtonAnime.Name = "radioButtonAnime";
+            this.radioButtonAnime.TabStop = true;
+            this.radioButtonAnime.UseVisualStyleBackColor = true;
+            this.radioButtonAnime.CheckedChanged += new System.EventHandler(this.radioButtonAnime_CheckedChanged);
+            // 
+            // radioButtonDefault
+            // 
+            resources.ApplyResources(this.radioButtonDefault, "radioButtonDefault");
+            this.radioButtonDefault.Name = "radioButtonDefault";
+            this.radioButtonDefault.UseVisualStyleBackColor = true;
+            this.radioButtonDefault.CheckedChanged += new System.EventHandler(this.radioButtonDefault_CheckedChanged);
+            // 
+            // animeVideoX2
+            // 
+            resources.ApplyResources(this.animeVideoX2, "animeVideoX2");
+            this.animeVideoX2.Name = "animeVideoX2";
+            this.animeVideoX2.UseVisualStyleBackColor = true;
+            this.animeVideoX2.CheckedChanged += new System.EventHandler(this.animeVideoX2_CheckedChanged);
+            // 
+            // animeVideoX4
+            // 
+            resources.ApplyResources(this.animeVideoX4, "animeVideoX4");
+            this.animeVideoX4.Name = "animeVideoX4";
+            this.animeVideoX4.UseVisualStyleBackColor = true;
+            this.animeVideoX4.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            resources.ApplyResources(this.radioButton2, "radioButton2");
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.TabStop = true;
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // formatChecked
+            // 
+            this.formatChecked.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            resources.ApplyResources(this.formatChecked, "formatChecked");
+            this.formatChecked.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.formatChecked.FormattingEnabled = true;
+            this.formatChecked.Items.AddRange(new object[] {
+            resources.GetString("formatChecked.Items"),
+            resources.GetString("formatChecked.Items1"),
+            resources.GetString("formatChecked.Items2")});
+            this.formatChecked.Name = "formatChecked";
+            this.formatChecked.TabStop = false;
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Name = "label3";
+            // 
+            // startButton
+            // 
+            resources.ApplyResources(this.startButton, "startButton");
+            this.startButton.ForeColor = System.Drawing.Color.Red;
+            this.startButton.Name = "startButton";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label4.Name = "label4";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // upscaleChecked
+            // 
+            this.upscaleChecked.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            resources.ApplyResources(this.upscaleChecked, "upscaleChecked");
+            this.upscaleChecked.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.upscaleChecked.FormattingEnabled = true;
+            this.upscaleChecked.Items.AddRange(new object[] {
+            resources.GetString("upscaleChecked.Items"),
+            resources.GetString("upscaleChecked.Items1"),
+            resources.GetString("upscaleChecked.Items2"),
+            resources.GetString("upscaleChecked.Items3")});
+            this.upscaleChecked.Name = "upscaleChecked";
+            this.upscaleChecked.TabStop = false;
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label5.Name = "label5";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label7.Name = "label7";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label6.Name = "label6";
+            // 
+            // linkLabel1
+            // 
+            resources.ApplyResources(this.linkLabel1, "linkLabel1");
+            this.linkLabel1.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label8
+            // 
+            resources.ApplyResources(this.label8, "label8");
+            this.label8.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label8.Name = "label8";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label9
+            // 
+            resources.ApplyResources(this.label9, "label9");
+            this.label9.ForeColor = System.Drawing.Color.Red;
+            this.label9.Name = "label9";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.upscaleChecked);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.formatChecked);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.animeVideoX4);
+            this.Controls.Add(this.animeVideoX2);
+            this.Controls.Add(this.radioButtonDefault);
+            this.Controls.Add(this.radioButtonAnime);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.picPath);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox picPath;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButtonAnime;
+        private System.Windows.Forms.RadioButton radioButtonDefault;
+        private System.Windows.Forms.RadioButton animeVideoX2;
+        private System.Windows.Forms.RadioButton animeVideoX4;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox formatChecked;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox upscaleChecked;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
